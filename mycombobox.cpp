b52257345e16d4031d09ddbb48d6282af91bc0a6
Dave <bauke.dawid@gmail.com>
2019-03-07 14:21:01 +0100

    Added class for combobox with label populated from sql query or QStringList

---
+++ mycombobox.cpp
@@ -0,0 +1,6 @@
+#include "mycombobox.h"
+
+MyComboBox::MyComboBox()
+{
+
+}
---
+++ mycombobox.h
@@ -0,0 +1,11 @@
+#ifndef MYCOMBOBOX_H
+#define MYCOMBOBOX_H
+
+
+class MyComboBox
+{
+public:
+	MyComboBox();
+};
+
+#endif // MYCOMBOBOX_H
